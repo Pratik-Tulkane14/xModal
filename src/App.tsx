fa426aa7f46034d94650ff8e6d460dd7b9ae5174
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2024-11-27 19:39:41 +0530

    fix code

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,23 @@ function App() {
   const [email, setEmail] = useState<string>('')
   const [phNumber, setPhNumber] = useState<string>('');
   const [dateOfBirth, setDateOfBirth] = useState<string>('');
-  const handleSubmit =(e:React.FormEvent)=>{
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if(phNumber?.length!=10){
+    if (phNumber?.length != 10) {
       window.alert("Invalid phone number. Please enter 10-digit phone number")
-      return 
+      return
 
     }
     const birthDate = new Date(dateOfBirth);
     const todayDate = new Date()
-     if(birthDate>=todayDate){
+    if (birthDate >= todayDate) {
       window.alert("Invalid date of birth. Date of birth can not be in the future.")
 
-    }else{
-       setUserName('');
-       setEmail('');
-       setPhNumber('');
-       setDateOfBirth('');
+    } else {
+      setUserName('');
+      setEmail('');
+      setPhNumber('');
+      setDateOfBirth('');
     }
   }
   const handleClickOutside = (e: MouseEvent) => {
@@ -45,49 +45,50 @@ function App() {
   }, [isModalOpen]);
   return (
     <>
-
-      <div className="modal">
+      <div className="main">
         <h1>User Details Modal</h1>
-        <button className='btn' onClick={()=>setIsModalOpen(!isModalOpen)}>Open Form</button>
+        <button className='btn' onClick={() => setIsModalOpen(!isModalOpen)}>Open Form</button>
+      </div>
+      <div className="modal">
         <div className="modal-content" ref={modalRef}>
           {isModalOpen &&
             <form className='form' onSubmit={handleSubmit} >
 
-            <h1>Fill Details</h1>
-            <div className="feild">
+              <h1>Fill Details</h1>
+              <div className="feild">
 
-            <label htmlFor="userName">Username:</label>
-            </div>
-            <div className="feild">
+                <label htmlFor="userName">Username:</label>
+              </div>
+              <div className="feild">
 
                 <input type="text" id='username' required value={userName} onChange={(e) => setUserName(e.target.value)} />
-            </div>
-            <div className="feild">
+              </div>
+              <div className="feild">
 
-            <label htmlFor="email">Email Address:</label>
-            </div>
-            <div className="feild">
+                <label htmlFor="email">Email Address:</label>
+              </div>
+              <div className="feild">
 
-            <input type="email" id='email' required value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div>
-            <div className="feild">
+                <input type="email" id='email' required value={email} onChange={(e) => setEmail(e.target.value)} />
+              </div>
+              <div className="feild">
 
-            <label htmlFor="phoneNumber">Phone Number:</label>
-            </div>
-            <div className="feild">
+                <label htmlFor="phoneNumber">Phone Number:</label>
+              </div>
+              <div className="feild">
 
                 <input type="text" id='phone' maxLength={10} required value={phNumber} onChange={(e) => setPhNumber(e.target.value)} />
-            </div>
-            <div className="feild">
+              </div>
+              <div className="feild">
 
-            <label htmlFor="dateOfBirth">Date of Brith:</label>
-            </div>
-            <div className="feild">
+                <label htmlFor="dateOfBirth">Date of Brith:</label>
+              </div>
+              <div className="feild">
 
                 <input type="date" id='dob' required value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} />
-            </div>
+              </div>
               <button type='submit' className='btn submit-button' >Submit</button>
-          </form>
+            </form>
           }
 
         </div>
